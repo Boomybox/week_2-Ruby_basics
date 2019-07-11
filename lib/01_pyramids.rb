043f80e5fbc1f0916f358793380b5203f6daa178
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-11 23:43:32 +0200

    Update 01_pyramids.rb

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,5 +1,6 @@
-
+#-------------------------------------------------------------
 #demie pyramide 
+#-------------------------------------------------------------
 
 def half_pyramid
 
@@ -26,39 +27,35 @@ symbol = "#"
 
 end
 
-
+#-------------------------------------------------------------
 #Pyramide entière
+#-------------------------------------------------------------
 
 def full_pyramid
-
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-print ">" 
-nb = gets.chomp.to_i
-
-  while nb %2==0                                #boucle qui demande à l'utilisateur de resaisir un nombre si la première saisie est paire.
-      puts "Oops ! Please enter odd number :"   # Oops ! veuillez saisir un nombre impair
-      print ">"
-      nb = gets.chomp.to_i
-  end
-
-i = 1
-ligne = 1
-symbol = "#"
-
-  while ligne < (nb/2) + 1               #tant que le nombre de lignes n'est pas égal à la moitié du nombre choisi arrondi au chiffre supérieur,
-        puts (symbol*i).center(nb*2)     #le nombre de # croit
-    i += 2
-    ligne += 1
-  end
   
-  while ligne > nb/2-1 && i>0           # à partir du moment ou le nombre de lignes a atteint la moitié du nombre choisi, le nombre de # décroit.
-        puts (symbol*i).center(nb*2)
-    i += -2
-    ligne += 1
+  symbol = "#"
+  nb =-1 # Initialisation de number pour rentrer dans la boucle
+  while nb <= 1 || nb > 25 do # tant que l'utilisateur ne choisit pas un nombre entre 1 et 25, on relance la boucle.
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+    print "> "
+    nb = gets.chomp.to_i
+    puts "Voici la pyramide : "
+    lign = 1
+    i=1
+  end
+
+  while lign <= nb
+    puts (symbol*i).center((nb*2)-1) # Centre les # , sur la valeur de la base de la pyramide
+    lign +=1
+    i +=2 # chaque itération ajout deux #
   end
 end
 
-full_pyramid 
+full_pyramid
+
+#-------------------------------------------------------------
+# wtf_pyramid
+#-------------------------------------------------------------
 
 def wtf_pyramid
 
