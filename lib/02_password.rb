556c88a9fd465a413cae5050fa2cb2b0b85f4233
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-11 23:46:50 +0200

    Update 02_password.rb

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,40 +1,30 @@
-require 'io/console' 
-
+require 'io/console' #bibliotheque qui permet de cacher le mot de passe lors de son écriture
 
 def signup
-
     puts "Définis ton mot de passe : "
-   
-    @mdp = STDIN.noecho(&:gets).chomp 
-
+    @mdp = STDIN.noecho(&:gets).chomp   #permet de cacher le mot de passe 
+    
 end
 
 def login
-
     puts "Saisis ton mot de passe : "
-    
-    log = STDIN.noecho(&:gets).chomp 
-    
-        while @mdp != log
-        puts "Mot de passe incorrect ! Retente ta chance : "
-        
-        log = STDIN.noecho(&:gets).chomp 
+    log = STDIN.noecho(&:gets).chomp    #permet de cacher le mot de passe 
 
+        while @mdp != log
+             puts "Mot de passe incorrect ! Retente ta chance : "        
+             log = STDIN.noecho(&:gets).chomp 
         end
 end
 
 
 def welcome_screen 
-
     puts "Bienvenue dans ton espace secret ! "
-
 end
 
 def perform 
         signup
         login
         welcome_screen
-
 end
 
 perform
